63278fef4cc6ff81885e25182d11ca983b65331d
Michael Soh <devnull@mikesoh.com>
2016-07-23 14:14:09 -0400

    Sample works; adding debugging statements

--- solution.py
+++ solution.py
@@ -13,7 +13,7 @@ def answer(input):
         num_search = re.match('\d', c)
 
         if (re.match('\d', c)):
-            print "Got number " + c + "; pushing to output";
+            print "Got number " + c + "; pushing to output" if DEBUG > 0
             output.append(c)
         elif (re.match('[\*\+]', c)):
             print "Got operator..."
@@ -25,30 +25,37 @@ def answer(input):
             elif (re.match('\+', c)):
                 current_operator = addition_operator
 
-            if ((len(operators) > 0) and (operators[0] == "*")):
-                compare_operator = 10
-            elif ((len(operators) > 0) and (operators[0] == "+")):
-                compare_operator = 5
-                
+            while (len(operators) > 0):
+                if (operators[len(operators) - 1] == "*"):
+                    compare_operator = 10
+                elif (operators[len(operators) - 1] == "+"):
+                    compare_operator = 5
 
-            if (current_operator <= compare_operator):
-                print "  (" + c + ") presendence is LESS THAN OR EQUAL TO operator at top of stack: " + operators[0]
-                print "    Pushing (" + operators[0] + ") to output..."
-                
-                output.append(operators.pop(0))
+                if (current_operator < compare_operator):
+                    print "  (" + c + ") presendence is LESS THAN OR EQUAL TO operator at top of stack: " + operators[0]
+                    print "    Pushing (" + operators[0] + ") to output..."
+                    output.append(operators.pop())
+                else:
+                    break
+            # END WHILE LOOP
 
             print "  Pushing (" + c + ") to OPERATOR stack..."
             operators.append(c)
     # END FOR LOOP
 
     output += operators
+    print "Output so far: "
     print output
+
+    
 
 
 # MAIN
+DEBUG = 0
 
 foo = "2+3*2"
-
 foo = "2*4*3+9*3+5"
 
 answer(foo)
+print foo
+print "Should be 243**93*5++"
