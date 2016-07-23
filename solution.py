68a183a8632c1d05e94884ef009067b20981b86a
Michael Soh <devnull@mikesoh.com>
2016-07-23 11:56:29 -0400

    Separate out numbers and operators.

--- solution.py
+++ solution.py
@@ -1,2 +1,23 @@
+import re
+
 def answer(str):
-    # your code here
+    print "GIVEN: " + str
+
+    for c in str:
+        num_search = re.match('\d', c)
+
+        if (re.match('\d', c)):
+            print "Got number: " + c
+        elif (re.match('\*', c)):
+            print "Got muliply"
+        elif (re.match('\+', c)):
+            print "Got plus"
+
+
+# MAIN
+
+foo = "2+3*2"
+
+#foo = "2*4*3+9*3+5"
+
+answer(foo)
