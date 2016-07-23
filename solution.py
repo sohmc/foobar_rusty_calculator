26289466f11af8f516ea65ed6394d47c29250150
Michael Soh <devnull@mikesoh.com>
2016-07-23 12:52:50 -0400

    Looks like things are working for simple lines.

--- solution.py
+++ solution.py
@@ -1,17 +1,48 @@
 import re
 
-def answer(str):
-    print "GIVEN: " + str
+def answer(input):
+    output = []
+    operators = []
+    
+    multiply_operator = 10
+    addition_operator = 5
 
-    for c in str:
+    print "GIVEN: " + input
+
+    for c in input:
         num_search = re.match('\d', c)
 
         if (re.match('\d', c)):
-            print "Got number: " + c
-        elif (re.match('\*', c)):
-            print "Got muliply"
-        elif (re.match('\+', c)):
-            print "Got plus"
+            print "Got number " + c + "; pushing to output";
+            output.append(c)
+        elif (re.match('[\*\+]', c)):
+            print "Got operator..."
+            current_operator = 0
+            compare_operator = 0
+
+            if (re.match('\*', c)):
+                current_operator = multiply_operator
+            elif (re.match('\+', c)):
+                current_operator = addition_operator
+
+            if ((len(operators) > 0) and (operators[0] == "*")):
+                compare_operator = 10
+            elif ((len(operators) > 0) and (operators[0] == "+")):
+                compare_operator = 5
+                
+
+            if (current_operator <= compare_operator):
+                print "  (" + current_operator + ") presendence is LESS THAN OR EQUAL TO operator at top of stack: " + str(compare_operator)
+                print "    Pushing to output..."
+                
+                output.append(operators.pop(0))
+
+            print "  Pushing (" + c + ") to OPERATOR stack..."
+            operators.append(c)
+    # END FOR LOOP
+
+    output += operators
+    print output
 
 
 # MAIN
