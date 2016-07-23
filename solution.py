16d24cf25b1915520392d08d1b76b93df3c8e123
Michael Soh <devnull@mikesoh.com>
2016-07-23 15:10:56 -0400

    Last commit didn't work.  WIth fixes.

--- solution.py
+++ solution.py
@@ -1,22 +1,24 @@
 import re
 
-def answer(input):
+def answer(str):
+#    DEBUG = 0  # For Submission
     output = []
     operators = []
     
     multiply_operator = 10
     addition_operator = 5
 
-    if DEBUG > 5: print "GIVEN: " + input
+    if DEBUG > 5: print "GIVEN: " + str
 
-    for c in input:
+    for c in str:
         num_search = re.match('\d', c)
 
         if (re.match('\d', c)):
             if DEBUG > 0: print "Got number " + c + "; pushing to output"
             output.append(c)
+            if DEBUG > 5: print output
         elif (re.match('[\*\+]', c)):
-            if DEBUG > 5: print "Got operator..."
+            if DEBUG > 0: print "Got operator (" + c + ")"
             current_operator = 0
             compare_operator = 0
 
@@ -31,7 +33,7 @@ def answer(input):
                 elif (operators[len(operators) - 1] == "+"):
                     compare_operator = 5
 
-                if (current_operator < compare_operator):
+                if (current_operator <= compare_operator):
                     if DEBUG > 5: print "  (" + c + ") presendence is LESS THAN OR EQUAL TO operator at top of stack: " + operators[0]
                     if DEBUG > 5: print "    Pushing (" + operators[0] + ") to output..."
                     output.append(operators.pop())
@@ -41,6 +43,7 @@ def answer(input):
 
             if DEBUG > 0: print "  Pushing (" + c + ") to OPERATOR stack..."
             operators.append(c)
+            if DEBUG > 5: print operators
     # END FOR LOOP
 
     output += operators
@@ -51,11 +54,11 @@ def answer(input):
 
 
 # MAIN
-DEBUG = 0
+DEBUG = 10
 
-foo = "2+3*2"
 foo = "2*4*3+9*3+5"
+foo = "2+3*5+1+2+7*4"
+foo = "3+4*2*1+5"
 
 answer(foo)
 if DEBUG > 0: print foo
-if DEBUG > 0: print "Should be 243**93*5++"
